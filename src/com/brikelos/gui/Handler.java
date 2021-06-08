8ad6b308c70b5906b3818edf0000c560594be05f
FranciscoDadone <dadonefrancisco@hotmail.com.ar>
2021-06-07 21:23:25 -0300

    inicios de la gui

--- src/com/brikelos/gui/Handler.java
+++ src/com/brikelos/gui/Handler.java
@@ -1,13 +1,9 @@
 package com.brikelos.gui;
 
-import com.sun.tools.javac.Main;
-
-import javax.swing.*;
-
 public class Handler {
 
     public void main() {
-        new MainGUI("App");
+        new MainGUI("Brikelos");
     }
 
 }
--- src/com/brikelos/gui/MainGUI.java
+++ src/com/brikelos/gui/MainGUI.java
@@ -1,10 +1,16 @@
 package com.brikelos.gui;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class MainGUI extends JFrame {
 
     private JPanel mainPanel;
+    private JButton NUEVOCLIENTEButton;
+    private JButton AGREGARVENTAButton;
+    private JButton VERCLIENTESButton;
+    private JPanel sidebar;
+    private JPanel contentPanel;
 
     public MainGUI(String title) {
         super(title);
@@ -13,7 +19,18 @@ public class MainGUI extends JFrame {
         this.setContentPane(mainPanel);
         this.pack();
         this.setExtendedState(JFrame.MAXIMIZED_BOTH);
-        this.setVisible(true);
-    }
+        this.setIconImage(Toolkit.getDefaultToolkit().getImage(getClass().getResource("./../images/logo.png")));
 
+        this.setVisible(true);
+
+        AGREGARVENTAButton.addActionListener(e -> {
+
+        });
+        VERCLIENTESButton.addActionListener(e -> {
+
+        });
+        NUEVOCLIENTEButton.addActionListener(e -> {
+
+        });
+    }
 }
